7d32ab74f73a78f5d505c9a12be10b1ef3520c31
GeoffroyK <geoffroy.keime@gmail.com>
2021-04-04 02:05:16 +0200

    finished gui + change makefile + cy logo

--- src/gui.c
+++ src/gui.c
@@ -1,23 +1,35 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include "memory.h"
+#include "batch.h"
+#include "util.h"
 
+GtkBuilder      *builder; 
+GtkWidget       *window;
+GtkWidget       *memoryRep;
+GtkFileChooser     *filechooser;
+
+int log_fd;
+
+char *filepath;
 
 int main(int argc, char *argv[])
 {
-    GtkBuilder      *builder; 
-    GtkWidget       *window;
-    GtkWidget       *memory;
-        
+    initLog(&log_fd, "gui.log");
+    
     gtk_init(&argc, &argv);
 
     builder = gtk_builder_new();
     gtk_builder_add_from_file (builder, "OS_gui.glade", NULL);
 
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+
+    memoryRep = GTK_WIDGET(gtk_builder_get_object(builder,"memory"));
+    filechooser = GTK_FILE_CHOOSER(gtk_builder_get_object(builder,"file_choser"));
+    gtk_file_chooser_set_local_only(filechooser, TRUE);
+
     gtk_builder_connect_signals(builder, NULL);
 
-    memory = GTK_WIDGET(gtk_builder_get_object(builder,"memory"));
 
     g_object_unref(builder);
 
@@ -27,46 +39,60 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void on_add_clicked(){
-	printf("Add block...");
-}
-
 void on_window_main_destroy() {
     gtk_main_quit();
 }
 
+void on_read_button_clicked() {
+    if(filepath != NULL) {
+        printf("%s\n",filepath);
+        batch(filepath);
+        gtk_widget_queue_draw(memoryRep);
+    }
+}
+
+void on_file_choser_file_set() {
+    filepath = gtk_file_chooser_get_filename(filechooser);
+    printf("Input file %s\n",filepath);
+}
 
 gboolean on_memory_draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
-	
 	guint width, height;
 
-    //int totalOfBlocks = getMemorySize();
-    //BlockList bl = getBlockList();
+    int totalOfBlocks = getMemorySize(memory);
+    BlockList bl = getBlockList(memory);
 
-    int totalOfBlocks = 100;
 	int size = 10;
     int positionX = 0;
     int positionY = 0;
+    int currentBlockSize = 0;
+    int blockIndice = 0;
 
 	width = gtk_widget_get_allocated_width(widget);
 	height = gtk_widget_get_allocated_height(widget);
 
     for(int i = 0; i < totalOfBlocks; i++) {
-        if(positionX == height) {
+        if(positionX >= height) {
             positionY += size + 1;
             positionX = 0;
         }
-        if(i % 2 == 0){
+        if(bl != NULL){
             cairo_rectangle (cr, positionX, positionY, size, size);
             cairo_set_source_rgba (cr, 1, 0, 0, 1);
             cairo_fill (cr);
-
+            blockIndice ++;
+            currentBlockSize = getBlockSize(bl);
         } else {
             cairo_rectangle (cr, positionX, positionY, size, size);
             cairo_set_source_rgba (cr, 1, 1, 1, 1);
             cairo_fill (cr);
-
         }
+
+        if(blockIndice == currentBlockSize && bl != NULL)  {
+           blockIndice = 0;
+           bl = getNextBlock(bl);
+        }
+
         positionX += size + 1;
     }
 	return FALSE;	
